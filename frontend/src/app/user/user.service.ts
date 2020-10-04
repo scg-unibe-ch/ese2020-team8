6fa3b67d9c74e64f59a5a0ae9928a03ef49afbd0
Jan Dietrich <jan.dietrich@tecpose.com>
2020-10-05 00:36:05 +0200

    fixed wrong backend url in user service

--- frontend/src/app/user/user.service.ts
+++ frontend/src/app/user/user.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { User } from "./user";
 import {HttpClient} from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  url = environment.endpointURL + '/user';
+
   constructor(
     private http: HttpClient
   ) { }
 
   register(user: User) {
-    return this.http.post('/user/register', user);
+    return this.http.post(this.url + '/register', user);
   }
 }
