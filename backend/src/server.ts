8bbcc9c059d4ccab8953360e1e593303ba992459
Simone Egli <simone.egli@protonmail.ch>
2020-10-07 18:04:10 +0200

    remove test comment II

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,4 @@
-import express, { Application , Request, Response } from 'express'; // re-add test comment
+import express, { Application , Request, Response } from 'express'; 
 import morgan from 'morgan';
 import { Sequelize } from 'sequelize';
 import { TodoList } from './models/todolist.model';
