013d1ce14e19461aae90f5b7869ce4c1fdcbc434
Jan Dietrich <jan.dietrich@tecpose.com>
2020-10-11 01:07:55 +0200

    Revert "Revert "added passwordStrengthTest frontend""
    This reverts commit 6aab1719e3d73a9929ab5008b1948a5a1dcec592.

--- frontend/src/app/user/register/register.component.ts
+++ frontend/src/app/user/register/register.component.ts
@@ -5,12 +5,10 @@ import {
   AbstractControl,
   FormGroup,
   ValidationErrors,
-  FormGroupDirective,
-  NgForm,
+  Validators,
 } from '@angular/forms';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
-import {ErrorStateMatcher} from '@angular/material/core';
 
 @core.Component({
   selector: 'app-register',
@@ -18,17 +16,18 @@ import {ErrorStateMatcher} from '@angular/material/core';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements core.OnInit {
-  registerForm = new FormGroup(
-    {
-      userName: new FormControl(''),
-      password: new FormControl(''),
-      // passwordConfirm: new FormControl(''),
-      passwordConfirm: new FormControl('', passwordRepeatValidatorOld('password')),
-    },
-    // {
-    //   validators: passwordRepeatValidator,
-    // }
-  );
+  registerForm = new FormGroup({
+    userName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(7),
+      passwordStrengthValidator,
+    ]),
+    passwordConfirm: new FormControl('', passwordRepeatValidator('password')),
+  });
 
   constructor(private router: Router, private userService: UserService) {}
 
@@ -49,31 +48,36 @@ export class RegisterComponent implements core.OnInit {
   }
 }
 
-export function passwordRepeatValidatorOld(fcSelector: string): ValidatorFn {
+export function passwordRepeatValidator(fcSelector: string): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } | null => {
     const fc = control.parent?.controls[fcSelector] || null;
-    return fc && fc.value !== control.value ? { passwordsDoNotMatch: {} } : null;
+    return fc && fc.value !== control.value
+      ? { passwordsDoNotMatch: {} }
+      : null;
   };
 }
-// export const passwordRepeatValidator: ValidatorFn = (
-//   control: FormGroup
-// ): ValidationErrors | null => {
-//   const pw = control.get('password');
-//   const pw2 = control.get('passwordConfirm');
-//   return pw &&
-//     pw2 &&
-//     (pw.dirty || pw.touched) &&
-//     (pw2.dirty || pw2.touched) &&
-//     pw.value !== pw2.value
-//     ? { passwordsDoNotMatch: true }
-//     : null;
-// };
-//
-// export class MyErrorStateMatcher implements ErrorStateMatcher {
-//   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-//     const invalidCtrl = !!(control && control.invalid && control.parent.dirty);
-//     const invalidParent = !!(control && control.parent && control.parent.invalid && control.parent.dirty);
-//
-//     return (invalidCtrl || invalidParent);
-//   }
-// }
+
+const passwordStrengthValidator = (
+  control: AbstractControl
+): ValidationErrors | null => {
+  const value: string = control.value || '';
+  if (!value) {
+    return null;
+  }
+
+  const upperCaseCharacters = /[A-Z]+/g;
+  const lowerCaseCharacters = /[a-z]+/g;
+  const numberCharacters = /[0-9]+/g;
+  const specialCharacters = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+  if (
+    upperCaseCharacters.test(value) === false ||
+    lowerCaseCharacters.test(value) === false ||
+    numberCharacters.test(value) === false ||
+    specialCharacters.test(value) === false
+  ) {
+    return {
+      passwordStrength:
+        'Password must contain at least two of the following: numbers, lowercase letters, uppercase letters, or special characters.',
+    };
+  }
+};
