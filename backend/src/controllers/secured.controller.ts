86d445f026319a0acd48853a24de43149003a236
Jan Dietrich <jan.dietrich@tecpose.com>
2020-10-11 23:58:18 +0200

    secure api

--- backend/src/controllers/secured.controller.ts
+++ backend/src/controllers/secured.controller.ts
@@ -1,14 +1,14 @@
 import express, { Router, Request, Response } from 'express';
-import { verifyToken } from '../middlewares/checkAuth';
+import { verifyToken, IAuthRequest } from '../middlewares/checkAuth';
 
 const securedEndpoint: Router = express.Router();
 
 // This is a middleware function that validates the token in the authorization-header for any incoming request
 securedEndpoint.use(verifyToken);
 
-securedEndpoint.get('/', (req: Request, res: Response) => {
+securedEndpoint.get('/', (req: IAuthRequest, res: Response) => {
     // for demonstration purposes the content of the token and a message is returned
-    res.send({message: `This is a secured  Endpoint, ${req.body.tokenPayload.userName}` , decodedToken: req.body.tokenPayload });
+    res.send({message: `This is a secured  Endpoint, ${req.user.userName}` , decodedToken: req.user });
 });
 
 export const SecuredController: Router = securedEndpoint;
