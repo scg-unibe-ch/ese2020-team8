86f69a7151f5df992448e9954e990bb017494bbd
Jan Dietrich <jan.dietrich@tecpose.com>
2020-10-11 15:16:52 +0200

    Added Backend Validation and user properties resolves #15 resolves #17

---
+++ backend/src/middlewares/checkPassword.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express';
+
+export function checkPassword(req: Request, res: Response, next: NextFunction) {
+  const password = req.body.password;
+
+  const upperCaseCharacters = /[A-Z]+/g;
+  const lowerCaseCharacters = /[a-z]+/g;
+  const numberCharacters = /[0-9]+/g;
+  const specialCharacters = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+  if (
+    upperCaseCharacters.test(password) === false ||
+    lowerCaseCharacters.test(password) === false ||
+    numberCharacters.test(password) === false ||
+    specialCharacters.test(password) === false
+  ) {
+    res.status(400).send({
+      passwordStrength:
+        'Password must contain at least two of the following:' +
+        ' numbers, lowercase letters, uppercase letters, or special characters.',
+    });
+  } else {
+    next();
+  }
+}
--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,10 +1,19 @@
-import { TodoItem, TodoItemAttributes, TodoItemCreationAttributes } from './todoitem.model';
 import { Optional, Model, Sequelize, DataTypes } from 'sequelize';
 
 export interface UserAttributes {
     userId: number;
     userName: string;
     password: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    isAdmin: boolean;
+    gender?: string;
+    phone?: string;
+    street?: string;
+    zip?: string;
+    city?: string;
+    country?: string;
 }
 
 export interface UserCreationAttributes extends Optional<UserAttributes, 'userId'> { }
@@ -13,6 +22,16 @@ export class User extends Model<UserAttributes, UserCreationAttributes> implemen
     userId!: number;
     userName!: string;
     password!: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    isAdmin: boolean;
+    gender?: string;
+    phone?: string;
+    street?: string;
+    zip?: string;
+    city?: string;
+    country?: string;
 
     public static initialize(sequelize: Sequelize) {
         User.init({
@@ -27,7 +46,52 @@ export class User extends Model<UserAttributes, UserCreationAttributes> implemen
             },
             password: {
                 type: DataTypes.STRING,
+                allowNull: false,
+                // validate: {
+                //     is: [
+                //         '/[A-Z]+/g',
+                //         '/[a-z]+/g',
+                //         '/[0-9]+/g',
+                //         '/[!@#$%^&*()_+\-=\[\]{};\':"\\|,.<>\/?]+/'
+                //     ]
+                // }
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    isEmail: true,
+                }
+            },
+            firstName: {
+                type: DataTypes.STRING,
                 allowNull: false
+            },
+            lastName: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            isAdmin: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false
+            },
+            gender: {
+                type: DataTypes.STRING
+            },
+            phone: {
+                type: DataTypes.STRING
+            },
+            zip: {
+                type: DataTypes.STRING
+            },
+            street: {
+                type: DataTypes.STRING
+            },
+            city: {
+                type: DataTypes.STRING
+            },
+            country: {
+                type: DataTypes.STRING
             }
         },
             {
