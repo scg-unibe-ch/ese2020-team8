2bdee52185595ad0facda93f909e133336bdd5d2
Judith Birrer <judith.birrer@students.unibe.ch>
2020-10-24 14:42:42 +0200

    show error message in frontend

--- frontend/src/app/user/user.service.ts
+++ frontend/src/app/user/user.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
-import { User } from "./user";
-import {HttpClient} from '@angular/common/http';
+import { User } from './user';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { map } from 'rxjs/operators';
-import {Observable} from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
   url = environment.endpointURL + '/user';
 
   token: string;
@@ -17,35 +17,38 @@ export class UserService {
   loggedIn: boolean;
   isAdmin: boolean;
 
-  constructor(
-    private http: HttpClient
-  ) {
+  constructor(private http: HttpClient, private _snackBar: MatSnackBar) {
     this.checkUserStatus();
   }
 
   register(user: User): Observable<any> {
-    return this.http.post(this.url + '/register', user);
+    return this.http
+      .post(this.url + '/register', user)
+      .pipe(catchError((err) => this.handleError(err)));
   }
 
   login(user: User): Observable<any> {
-    return this.http.post(this.url + '/login', {
-      userName: user.userName,
-      password: user.password
-    })
-    .pipe(
-      map( (res: {
-        token: string;
-        user: {
-          userName: string;
-        };
-      })  => {
-             // Set user data in local storage
-        localStorage.setItem('userToken', res.token);
-        localStorage.setItem('userName', res.user.userName);
-
-        this.checkUserStatus();
+    return this.http
+      .post(this.url + '/login', {
+        userName: user.userName,
+        password: user.password,
       })
-    );
+      .pipe(
+        map(
+          (res: {
+            token: string;
+            user: {
+              userName: string;
+            };
+          }) => {
+            // Set user data in local storage
+            localStorage.setItem('userToken', res.token);
+            localStorage.setItem('userName', res.user.userName);
+
+            this.checkUserStatus();
+          }
+        )
+      );
   }
 
   logout(): void {
@@ -68,10 +71,41 @@ export class UserService {
     }
 
     // Set boolean whether a user is logged in or not
-    this.loggedIn = !!(this.token);
+    this.loggedIn = !!this.token;
   }
 
   accessSecuredEndpoint(): Observable<any> {
     return this.http.get(environment.endpointURL + '/secured');
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+      this._snackBar.open('Backend not running?' + error.error.message);
+    } else if (
+      error.error.hasOwnProperty('name') &&
+      ['SequelizeValidationError', 'SequelizeUniqueConstraintError'].includes(
+        error.error.name
+      )
+    ) {
+      console.error(
+        `Backend returned error: ${error.error.errors[0].message} `
+      );
+      this._snackBar.open(
+        `Backend returned error: ${error.error.errors[0].message} `
+      );
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+      this._snackBar.open(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    // Return an observable with a user-facing error message.
+    return throwError('Something bad happened; please try again later.');
   }
 }
