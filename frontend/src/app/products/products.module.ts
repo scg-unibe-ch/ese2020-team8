af752de551eca834dc30ff1c6148c6d721dbc74f
Manuel Bühler <manuel_buehler@gmx.net>
2020-11-18 00:40:54 +0100

    added notification frontend implementation

--- frontend/src/app/products/products.module.ts
+++ frontend/src/app/products/products.module.ts
@@ -32,6 +32,7 @@ import {FilterComponent} from './filter/filter.component';
 import { ProductFilterPipe } from './pipes/product-filter.pipe';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import {MatBadgeModule} from '@angular/material/badge';
+import { NotificationComponent } from './notification/notification.component';
 
 
 @NgModule({
@@ -50,6 +51,7 @@ import {MatBadgeModule} from '@angular/material/badge';
     PayComponent,
     FilterComponent,
     ProductFilterPipe,
+    NotificationComponent,
   ],
   imports: [
     CommonModule,
