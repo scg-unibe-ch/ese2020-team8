eb8e8a588f3d04d7b29195afeafd7a15ddfeae74
Manuel Bühler <manuel_buehler@gmx.net>
2020-11-25 21:50:27 +0100

    resolved merge conflict

--- frontend/src/app/products/show/show.component.ts
+++ frontend/src/app/products/show/show.component.ts
@@ -1,6 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { IProduct } from '../products.service';
 import { environment } from 'src/environments/environment';
+import { ProductsService } from '../products.service';
+import { IQuestion, QuestionsService } from '../questions.service';
+import { UserService } from 'src/app/user/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
+
+
+
+
 
 @Component({
   selector: 'app-product-show',
@@ -10,15 +19,28 @@ import { environment } from 'src/environments/environment';
 export class ShowComponent implements OnInit {
 
   pageType: string;
+  productId: string;
+  panelOpenState = false;
+  questions: IQuestion[] = [];
+
 
   @Input() product: IProduct;
   photos: {imageSource: string; }[];
   slides: {image: string; }[];
 
   constructor(
-  ) { }
+    private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductsService,
+    private location: Location,
+    public userService: UserService,
+    private questionService: QuestionsService,
+  ) {  }
+
 
   ngOnInit(): void {
+    const productId = this.route.snapshot.paramMap.get('id');
+    this.questionService.getQuestionsPerProduct(productId).subscribe( questions => this.questions = questions );
       this.photos = this.product.Photos.map((photo) => {
         const newPhoto = {
           imageSource: `${environment.endpointURL}/images/${photo.fileName}`,
@@ -33,4 +55,13 @@ export class ShowComponent implements OnInit {
     this.product.productType === 'good' &&
     this.product.purchaseType  === 'rent';
   }
+
+  goToQuestion(product: IProduct): void {
+    this.router.navigate(['products', product.id, 'question-form']);
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
 }
