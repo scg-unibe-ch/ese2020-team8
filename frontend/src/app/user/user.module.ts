770caa7f499423db20a2ee0402568fbc7434b1df
Manuel Bühler <manuel_buehler@gmx.net>
2020-12-01 18:49:48 +0100

    improvements in register form

--- frontend/src/app/user/user.module.ts
+++ frontend/src/app/user/user.module.ts
@@ -11,6 +11,9 @@ import {ReactiveFormsModule, FormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {HttpClientModule} from '@angular/common/http';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
 
 @NgModule({
   declarations: [
@@ -26,7 +29,9 @@ import {MatMenuModule} from '@angular/material/menu';
     MatCardModule,
     MatButtonModule,
     MatInputModule,
-    MatMenuModule
+    MatMenuModule,
+    MatIconModule,
+    MatTooltipModule
   ]
 })
 export class UserModule { }
