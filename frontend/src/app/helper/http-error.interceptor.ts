16719552be43020d8fc454687313ada71183e555
Simone Egli <simone.egli@protonmail.ch>
2020-12-02 15:46:46 +0100

    implement action on snack bar for NotLoggedIn error

--- frontend/src/app/helper/http-error.interceptor.ts
+++ frontend/src/app/helper/http-error.interceptor.ts
@@ -9,10 +9,11 @@ import {
 import { Observable } from 'rxjs';
 import { tap, finalize } from 'rxjs/operators';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
-  constructor(private snackBar: MatSnackBar) {}
+  constructor(private snackBar: MatSnackBar, private router: Router) {}
 
   intercept(
     request: HttpRequest<unknown>,
@@ -21,12 +22,35 @@ export class HttpErrorInterceptor implements HttpInterceptor {
     let error;
     return next.handle(request).pipe(
       tap(null, (err: HttpErrorResponse) => (error = err)),
-      finalize(() =>
-        error
-          ? this.snackBar.open(error.message, 'close', {
+      finalize(() => {
+        if (error) {
+          switch (error.type) {
+            case 'BuyOwnProduct' : {
+
+            }
+            case 'NotYourProduct' : {
+
+            }
+            case 'NotAdmin' : {
+
+            }
+            case 'NotLoggedIn' : {
+              this.snackBar.open(error.message, 'Log in', {
+                duration: 5000,
+              })
+              .onAction().subscribe(
+                () => {
+                  this.router.navigate(['login'])
+                }
+              );
+            }
+            default : this.snackBar.open(error.message, 'close', {
               duration: 5000,
             })
-          : null
+          }
+        }
+        else { null }
+      }
       )
     );
   }
